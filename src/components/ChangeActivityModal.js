91b571e901ff3b17ab8e04f142b80983230f6900
zirow965 <192042@student.kcst.edu.kw>
2024-06-09 08:05:12 +0300

    update

--- src/components/ChangeActivityModal.js
+++ src/components/ChangeActivityModal.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import BaseModal from './BaseModal';
+import SelectActivity from "@/components/SelectActivity";
 
 const ChangeActivityModal = ({ isOpen, onClose, onSubmit, companyId }) => {
 	const [newActivity, setNewActivity] = useState('');
@@ -14,15 +15,7 @@ const ChangeActivityModal = ({ isOpen, onClose, onSubmit, companyId }) => {
 			<div className="grid gap-4 mb-4 grid-cols-1">
 				<div>
 					<label htmlFor="newActivity" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Activity</label>
-					<input
-						type="text"
-						id="newActivity"
-						name="newActivity"
-						value={newActivity}
-						onChange={(e) => setNewActivity(e.target.value)}
-						className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-						required
-					/>
+			<SelectActivity id="newActivity" label="New Activity" onActivityChange={setNewActivity}/>
 				</div>
 			</div>
 		</BaseModal>
